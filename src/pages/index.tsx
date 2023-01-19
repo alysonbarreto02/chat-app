132851d51714bf58c5c5c7d250e71d14aee587a7
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-01-19 18:16:32 -0300

    feat: :fire: definindo medidas da area  do chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,22 @@ import { SearchInput } from "./SearchInput";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-gray-dracula">
-      <div className="w-[400px] h-full py-5 px-10">
+    <div className="w-full h-screen flex bg-gray-dracula">
+      <div className="w-[500px] h-full py-5 px-3">
         <SearchInput />
         <div className="mt-4">
           <CardContact />
         </div>
       </div>
+      <div className="w-full h-full rounded-xl py-5 px-3">
+        <div className="w-full h-16 bg-black rounded-t-xl"></div>
+        <div className="bg-red-500 w-full h-[490px]">
+
+        </div>
+        <div className="w-full h-12 rounded-b-xl bg-green-dracula"> 
+
+        </div>
+      </div>
     </div>
   );
 }
-
