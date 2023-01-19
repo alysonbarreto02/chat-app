f0f9c9b160d971d2662e559e1a27a4d8bbd0334a
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-01-19 18:00:46 -0300

    feat: :fire: criando parte de contatos do site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import { CardContact } from "../components/CardContact";
 import { SearchInput } from "./SearchInput";
 
 export default function Home() {
   return (
     <div className="w-full h-screen bg-gray-dracula">
-      <div className="w-4/12 h-full py-5 px-10">
+      <div className="w-[400px] h-full py-5 px-10">
         <SearchInput />
+        <div className="mt-4">
+          <CardContact />
+        </div>
       </div>
     </div>
   );
