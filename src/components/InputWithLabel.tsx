16538294878492251ae3d16eb1577dcc227a58d3
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-01-23 20:51:22 -0300

    feat: :fire: implementando verificação de login

--- src/components/InputWithLabel.tsx
+++ src/components/InputWithLabel.tsx
@@ -1,10 +1,16 @@
+import { Dispatch, SetStateAction } from "react";
+
+type InputWithLabelProps = {
+  title: string;
+  placeholder: string;
+  getValueInput: Dispatch<SetStateAction<string>>;
+};
+
 export function InputWithLabel({
   title,
   placeholder,
-}: {
-  title: string;
-  placeholder: string;
-}) {
+  getValueInput,
+}: InputWithLabelProps) {
   return (
     <div>
       <div>
@@ -19,6 +25,7 @@ export function InputWithLabel({
       <input
         type="text"
         name={title}
+        onChange={(e) => getValueInput(e.target.value)}
         placeholder={placeholder}
         className="border border-gray-400 p-2 rounded-md w-80 bg-gray-dracula-clear"
       />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,34 @@
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 import { InputWithLabel } from "../components/InputWithLabel";
 
+const users = [{ username: "AlysonBarreto", password: "201275" }];
+
+
+
 export default function Login() {
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const confirmDetails = () => {
+    const filteredUser = users.filter((user) => userName === user.username)[0];
+    userName === filteredUser?.username || password === filteredUser?.password
+      ? console.log(true)
+      : console.log(false)
+  };
+
   return (
     <div className="w-full h-screen flex bg-gray-dracula">
       <div className="w-1/2 h-full  bg-gray-dracula">
         <div className="w-full h-20 flex items-center gap-2 bg-buffy">
-          <Icon icon="ic:twotone-chat-bubble" className="w-10 h-10 ml-4 text-pink-dracula" />
-          <p className="font-medium text-lg mb-2 text-green-dracula ">Chat APP</p>
+          <Icon
+            icon="ic:twotone-chat-bubble"
+            className="w-10 h-10 ml-4 text-pink-dracula"
+          />
+          <p className="font-medium text-lg mb-2 text-green-dracula ">
+            Chat APP
+          </p>
         </div>
         <div className="w-full h-4/5 flex flex-col justify-center items-center gap-7">
           <div className="text-center flex flex-col gap-2 text-pink-dracula">
@@ -15,16 +36,27 @@ export default function Login() {
             <p className="text-xs">Please enter your details</p>
           </div>
           <div className="flex flex-col gap-4">
-            <InputWithLabel title="Email" placeholder="Insira seu email" />
-            <InputWithLabel title="Senha" placeholder="Insira sua senha" />
+            <InputWithLabel
+              title="Email"
+              placeholder="Insira seu email"
+              getValueInput={setUserName}
+            />
+            <InputWithLabel
+              title="Senha"
+              placeholder="Insira sua senha"
+              getValueInput={setPassword}
+            />
           </div>
-          <button className="bg-green-dracula w-80 h-10 rounded-md text-gray-dracula">
+          <button
+            onClick={() => confirmDetails()}
+            className="bg-green-dracula w-80 h-10 rounded-md text-gray-dracula"
+          >
             Entrar
           </button>
         </div>
       </div>
       <div className="w-1/2 h-full flex items-center justify-center bg-gray-dracula-clear border-8 rounded-lg border-gray-dracula">
-        <img src="./Welcome.svg" alt="" className="w-60 h-60"/>
+        <img src="./Welcome.svg" alt="" className="w-60 h-60" />
       </div>
     </div>
   );
